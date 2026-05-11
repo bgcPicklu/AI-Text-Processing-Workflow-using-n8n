d6ce5242df4b2ed3a7e52d7bd21435ec2e46cae3
bgcPicklu <shoumendatasoftbd@gmail.com>
2026-05-11 22:27:31 +0600

    Completed along with demo video

--- backend/main.py
+++ backend/main.py
@@ -4,31 +4,49 @@ import uuid
 import requests
 import os
 from dotenv import load_dotenv
+from pathlib import Path
 
-load_dotenv()
+# Load .env correctly
+BASE_DIR = Path(__file__).resolve().parent
+env_path = BASE_DIR / ".env"
+load_dotenv(dotenv_path=env_path)
 
 app = FastAPI()
 
 N8N_WEBHOOK_URL = os.getenv("N8N_WEBHOOK_URL")
 
+print("N8N_WEBHOOK_URL:", N8N_WEBHOOK_URL)
+
+
+# Request model
 class UserRequest(BaseModel):
     email: str
     text: str
 
+
 @app.post("/process")
-def process_text(data:UserRequest):
+def process_text(data: UserRequest):
+
+    if not N8N_WEBHOOK_URL:
+        return {"error": "Webhook URL not found in .env"}
+
     session_id = str(uuid.uuid4())
 
-    paylod = {
-        'email': data.email,
-        'text' : data.text,
-        'session_id' : session_id
+    payload = {
+        "email": data.email,
+        "text": data.text,
+        "session_id": session_id
     }
 
-    response = requests.post(N8N_WEBHOOK_URL, json=paylod)
+    try:
+        response = requests.post(N8N_WEBHOOK_URL, json=payload)
+        return {
+            "message": "Workflow triggered successfully",
+            "session_id": session_id,
+            "n8n_response": response.text
+        }
 
-    return {
-        'message': 'Workflow triggered',
-        'session_id': session_id,
-        'n8n_response': response.text
-    }
+    except Exception as e:
+        return {
+            "error": str(e)
+        }
--- frontend/app.py
+++ frontend/app.py
@@ -1,24 +1,32 @@
 import streamlit as st
 import requests
 
-API_URL = 'http://127.0.0.1:8000/process'
+API_URL = "http://127.0.0.1:8000/process"
 
-st.title('AI Text Processing Workflow')
-email = st.text_input('Enter Email')
-text = st.text_area('Enter Text')
+st.title("AI Text Processing Workflow")
 
-if st.button('Submit'):
-    payload = {
-        'email': email,
-        'text': text
-    }
+email = st.text_input("Enter Email")
+text = st.text_area("Enter Text")
 
-    response = requests.post(API_URL, json = payload)
-    
-    if response.status_code == 200:
-        st.success('Workflow triggered successfully!')
-        st.json(response.json())
-    
+if st.button("Submit"):
+
+    if not email or not text:
+        st.warning("Please fill all fields")
     else:
-        st.error('Something went wrong')
-        
+        payload = {
+            "email": email,
+            "text": text
+        }
+
+        try:
+            response = requests.post(API_URL, json=payload)
+
+            if response.status_code == 200:
+                st.success("Workflow triggered successfully")
+                st.json(response.json())
+            else:
+                st.error("Request failed")
+                st.write(response.text)
+
+        except Exception as e:
+            st.error(str(e))
